410f29f2fc08e8701105ee29d1a691804a931bf2
Mark Hill <mark@drfcx.com>
2018-04-09 16:00:11 -0700

    added ssh public keys back as symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -8,7 +8,9 @@ ln -sf "$PWD/pam/pam_environment" ~/.pam_environment
 ln -sf "$PWD/profile" ~/.profile
 command -v bash 2>&1 > /dev/null && ln -sf "$PWD/bash/bashrc" ~/.bashrc
 command -v nvim 2>&1 > /dev/null || command -v vim 2>&1 > /dev/null && ln -sf "$PWD/nvim/init.vim" ~/.vimrc
-command -v ssh 2>&1 > /dev/null && mkdir -p ~/.ssh && ln -sf "$PWD/ssh/config" ~/.ssh/config
+command -v ssh 2>&1 > /dev/null && mkdir -p ~/.ssh \
+    && (for FILE in $(find "$PWD/ssh" -name '*.pub' -o -name 'config'); \
+        do ln -sf "$FILE" ~/.ssh/; done)
 command -v gpg 2>&1 > /dev/null \
     && [ -d ~/.gnupg ] \
     && (for FILE in $(find "$PWD/gnupg" -name '*.conf'); \
