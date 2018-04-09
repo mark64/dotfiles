6155657c08eac648fbaa00eb6c8051c79b3a4404
Mark Hill <mark@drfcx.com>
2018-04-09 00:24:54 -0700

    fixed bug with symlinks.sh, added minicom config

--- symlinks.sh
+++ symlinks.sh
@@ -4,6 +4,7 @@ HASNVIM=$(which nvim)
 HASVIM=$(which vim)
 HASSSH=$(which ssh)
 HASGPG=$(which gpg)
+HASMINICOM=$(which minicom)
 
 if [ ! -f "$PWD/$0" ]; then
     printf "error: this script must be run from the config repo subdirectory or else the symlinks won't work\n"
@@ -12,21 +13,25 @@ fi
 
 ln -sf "$PWD/profile" ~/.profile
 
-if [[ ! "$HASBASH" ]]; then
+if [ "$HASBASH" ]; then
     ln -sf "$PWD/bash/bashrc" ~/.bashrc
 fi
 
-if [[ ! "$HASVIM" ]] && [[ -z "$HASNVIM" ]]; then
+if [ "$HASVIM" ] && [ ! "$HASNVIM" ]; then
     ln -sf "$PWD/nvim/init.vim" ~/.vimrc
 fi
 
-if [[ ! "$HASSSH" ]]; then
+if [ "$HASSSH" ]; then
     mkdir -p ~/.ssh
     ln -sf "$PWD/ssh/config" ~/.ssh/config
 fi
 
-if [[ -z "$HASGPG" ]]; then
+if [ "$HASGPG" ]; then
     ln -sf "$PWD/gnupg" ~/.gnupg
+fi
+
+if [ "$HASMINICOM" ]; then
+    ln -sf "$PWD/minicom/config" ~/.minirc.dfl
 fi
 
 printf "success\nremember to source ~/.profile\n"
