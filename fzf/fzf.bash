91debdb3e20b063b280f996501c514cdf27044a3
Mark Hill <mark@drfcx.com>
2018-04-09 16:23:47 -0700

    fixed hardcoded path in fzf.bash

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/mark/.config/nvim/bundle/fzf/bin* ]]; then
-  export PATH="$PATH:/home/mark/.config/nvim/bundle/fzf/bin"
+if [[ ! "$PATH" == *$XDG_CONFIG_HOME/nvim/bundle/fzf/bin* ]]; then
+  export PATH="$PATH:$XDG_CONFIG_HOME/nvim/bundle/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/mark/.config/nvim/bundle/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$XDG_CONFIG_HOME/nvim/bundle/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/mark/.config/nvim/bundle/fzf/shell/key-bindings.bash"
+source "$XDG_CONFIG_HOME/nvim/bundle/fzf/shell/key-bindings.bash"
 
