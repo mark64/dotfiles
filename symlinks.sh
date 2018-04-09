dc9df546b0b9736fa87bc4409baba3e2530d6b03
Mark Hill <mark@drfcx.com>
2018-04-09 14:49:40 -0700

    added better gnupg support

--- symlinks.sh
+++ symlinks.sh
@@ -8,8 +8,9 @@ ln -sf "$PWD/pam/pam_environment" ~/.pam_environment
 ln -sf "$PWD/profile" ~/.profile
 command -v bash 2>&1 > /dev/null && ln -sf "$PWD/bash/bashrc" ~/.bashrc
 command -v nvim 2>&1 > /dev/null || command -v vim 2>&1 > /dev/null && ln -sf "$PWD/nvim/init.vim" ~/.vimrc
-command -v ssh 2>&1 > /dev/null && mkdir -p ~/.ssh && ln -sf "$PWD/ssh/config" ~/.ssh/config && cp $(find ssh -name '*.pub') ~/.ssh
-command -v gpg 2>&1 > /dev/null && ln -sf "$PWD/gnupg" ~/.gnupg
+command -v ssh 2>&1 > /dev/null && mkdir -p ~/.ssh && ln -sf "$PWD/ssh/config" ~/.ssh/config
+command -v gpg 2>&1 > /dev/null \
+    && [ -d ~/.gnupg ] \
+    && (for FILE in $(find "$PWD/gnupg" -name '*.conf'); \
+        do ln -sf "$FILE" ~/.gnupg/; done)
 command -v minicom 2>&1 > /dev/null && ln -sf "$PWD/minicom/config" ~/.minirc.dfl
-
-printf "success\nremember to source ~/.profile\n"
