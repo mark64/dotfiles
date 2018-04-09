29a3e310ffa6c0928ccfeb2c11a52675b1c3f45a
Mark Hill <mark@drfcx.com>
2018-04-09 00:59:42 -0700

    better POSIX compatibility for the symlink script

--- symlinks.sh
+++ symlinks.sh
@@ -1,37 +1,14 @@
 #!/usr/bin/env sh
-HASBASH=$(which bash)
-HASNVIM=$(which nvim)
-HASVIM=$(which vim)
-HASSSH=$(which ssh)
-HASGPG=$(which gpg)
-HASMINICOM=$(which minicom)
-
 if [ ! -f "$PWD/$0" ]; then
     printf "error: this script must be run from the config repo subdirectory or else the symlinks won't work\n"
     exit 1
 fi
 
 ln -sf "$PWD/profile" ~/.profile
-
-if [ "$HASBASH" ]; then
-    ln -sf "$PWD/bash/bashrc" ~/.bashrc
-fi
-
-if [ "$HASVIM" ] && [ ! "$HASNVIM" ]; then
-    ln -sf "$PWD/nvim/init.vim" ~/.vimrc
-fi
-
-if [ "$HASSSH" ]; then
-    mkdir -p ~/.ssh
-    ln -sf "$PWD/ssh/config" ~/.ssh/config
-fi
-
-if [ "$HASGPG" ]; then
-    ln -sf "$PWD/gnupg" ~/.gnupg
-fi
-
-if [ "$HASMINICOM" ]; then
-    ln -sf "$PWD/minicom/config" ~/.minirc.dfl
-fi
+command -v bash &> /dev/null && ln -sf "$PWD/bash/bashrc" ~/.bashrc
+command -v nvim &> /dev/null || command -v vim &> /dev/null && ln -sf "$PWD/nvim/init.vim" ~/.vimrc
+command -v ssh &> /dev/null && mkdir -p ~/.ssh && ln -sf "$PWD/ssh/config" ~/.ssh/config && cp "$PWD/ssh/*.pub" ~/.ssh/*.pub
+command -v gpg &> /dev/null && ln -sf "$PWD/gnupg" ~/.gnupg
+command -v minicom &> /dev/null && ln -sf "$PWD/minicom/config" ~/.minirc.dfl
 
 printf "success\nremember to source ~/.profile\n"
