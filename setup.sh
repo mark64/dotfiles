5c04afd1c5b01b920bebfb7c34f439aed7f04c3f
Mark Hill <mark@drfcx.com>
2018-04-09 17:54:13 -0700

    added authorized_keys and autoupdates for Vundle

--- setup.sh
+++ setup.sh
@@ -4,15 +4,21 @@ if [ ! -f "$PWD/$0" ]; then
     exit 1
 fi
 
+command -v git 2>&1 > /dev/null && git pull && git submodule update --init
+
 sed "s,XDG_CONFIG_HOME=\${HOME}/.config,XDG_CONFIG_HOME=$PWD," "$PWD/profile" >~/.profile
 ln -sf "$PWD/pam/pam_environment" ~/.pam_environment
 command -v bash 2>&1 > /dev/null && ln -sf "$PWD/bash/bashrc" ~/.bashrc
 command -v nvim 2>&1 > /dev/null || command -v vim 2>&1 > /dev/null && ln -sf "$PWD/nvim/init.vim" ~/.vimrc
 command -v ssh 2>&1 > /dev/null && mkdir -p ~/.ssh \
-    && (for FILE in $(find "$PWD/ssh" -name '*.pub' -o -name 'config'); \
+    && (for FILE in $(find "$PWD/ssh" -name authorized_keys -o -name '*.pub' -o -name 'config'); \
         do ln -sf "$FILE" ~/.ssh/; done)
 command -v gpg 2>&1 > /dev/null \
     && [ -d ~/.gnupg ] \
     && (for FILE in $(find "$PWD/gnupg" -name '*.conf'); \
         do ln -sf "$FILE" ~/.gnupg/; done)
 command -v minicom 2>&1 > /dev/null && ln -sf "$PWD/minicom/config" ~/.minirc.dfl
+
+command -v vim 2>&1 > /dev/null && vim -i NONE -c PluginInstall -c quitall 2>&1 > /dev/null
+command -v vim 2>&1 > /dev/null && vim -i NONE -c PluginUpdate -c quitall 2>&1 > /dev/null &
+command -v vim 2>&1 > /dev/null && vim -i NONE -c PluginClean -c quitall 2>&1 > /dev/null &
