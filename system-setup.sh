c02a68d7c141208f4a36cc90079748d31f15d09a
Mark Hill <markleehill@gmail.com>
2018-12-10 22:37:40 -0800

    Start implementing system-setup.sh

--- system-setup.sh
+++ system-setup.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env sh
 # This script installs packages and performs system-level modifications
-# TODO: implement
+# Only supports debian for now
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Must be run as root"
+    exit 1
+fi
+
+apt-get update
+apt-get -y full-upgrade
+apt-get -y install neovim build-essential cmake clang clang-tidy clang-format python3-numpy gnupg scdaemon gnupg-agent \
+    tmux curl openssh-client openssh-server python3-dev python-dev autojump bash-completion \
+    unattended-upgrades sshfs chromium openvpn
